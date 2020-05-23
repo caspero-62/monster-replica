435e4ee460d5e43d7f550243c17ea0e8b226c9b1
root <nwaforuchechukwu2007@gmail.com>
2020-05-23 14:59:29 +0100

    Updates

---
+++ precache-manifest.7dd617fa9d545bcd6c706cf44951cba8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec275e1bf69d0ecdd268d2ffaddacd99",
+    "url": "/monster-replica/index.html"
+  },
+  {
+    "revision": "666ed9959c56c14b04cd",
+    "url": "/monster-replica/static/css/main.122d9160.chunk.css"
+  },
+  {
+    "revision": "e1a9ce3e3cc11fda20a5",
+    "url": "/monster-replica/static/js/2.bda8a786.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-replica/static/js/2.bda8a786.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "666ed9959c56c14b04cd",
+    "url": "/monster-replica/static/js/main.b8c5472f.chunk.js"
+  },
+  {
+    "revision": "484bce1e04d3333c11bb",
+    "url": "/monster-replica/static/js/runtime-main.7324cb53.js"
+  }
+]);
